f4c2a0d727db70e8253f678eeb08aeff988ab951
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-15 22:56:50 -0300

    test: verify password encryption

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,11 +1,25 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'João Victor Vieira de Andrade',
+      email: 'joaovictorvieira.23@hotmail.com',
+      password: '2949249',
+    });
+
+    const compareHash = await bcrypt.compare('2949249', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should be able to register', async () => {
@@ -14,7 +28,7 @@ describe('User', () => {
       .send({
         name: 'João Victor Vieira de Andrade',
         email: 'joaovictorvieira.23@hotmail.com',
-        password_hash: '2949249',
+        password: '2949249',
       });
 
     expect(response.body).toHaveProperty('id');
@@ -26,7 +40,7 @@ describe('User', () => {
       .send({
         name: 'João Victor Vieira de Andrade',
         email: 'joaovictorvieira.23@hotmail.com',
-        password_hash: '2949249',
+        password: '2949249',
       });
 
     const response = await request(app)
@@ -34,7 +48,7 @@ describe('User', () => {
       .send({
         name: 'João Victor Vieira de Andrade',
         email: 'joaovictorvieira.23@hotmail.com',
-        password_hash: '2949249',
+        password: '2949249',
       });
 
     expect(response.status).toBe(400);
